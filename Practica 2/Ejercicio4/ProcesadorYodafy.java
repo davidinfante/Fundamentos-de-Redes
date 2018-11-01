e3c4fb9cd7aca7747fd141012dc6b11d8818a4dc
Ángel Gómez <aagomezies@gmail.com>
2018-11-02 00:39:37 +0100

    ej4

---
+++ Practica 2/Ejercicio4/ProcesadorYodafy.java
@@ -0,0 +1,94 @@
+package Yodafy;
+
+//
+// YodafyServidorIterativo
+// (CC) jjramos, 2012
+//
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Random;
+
+
+//
+// Nota: si esta clase extendiera la clase Thread, y el procesamiento lo hiciera el método "run()",
+// ¡Podríamos realizar un procesado concurrente! 
+//
+public class ProcesadorYodafy extends Thread {
+	// Referencia a un socket para enviar/recibir las peticiones/respuestas
+	private Socket socketServicio;
+	// stream de lectura (por aquí se recibe lo que envía el cliente)
+	private InputStream inputStream;
+	// stream de escritura (por aquí se envía los datos al cliente)
+	private OutputStream outputStream;
+        
+        private PrintWriter printWriter;
+        
+        private BufferedReader buffReader;
+	
+	// Para que la respuesta sea siempre diferente, usamos un generador de números aleatorios.
+	private Random random;
+	
+	// Constructor que tiene como parámetro una referencia al socket abierto en por otra clase
+	public ProcesadorYodafy(Socket socketServicio) {
+		this.socketServicio=socketServicio;
+		random=new Random();
+	}
+	
+        @Override 
+        public void run(){
+            procesa();
+        }
+	
+	// Aquí es donde se realiza el procesamiento realmente:
+	void procesa(){
+		
+		try {
+			// Obtiene los flujos de escritura/lectura
+			inputStream=socketServicio.getInputStream();
+			outputStream=socketServicio.getOutputStream();
+            printWriter = new PrintWriter(outputStream, true);
+            buffReader = new BufferedReader(new InputStreamReader(inputStream));
+                        
+			// Yoda hace su magia:
+			// Creamos un String a partir de la linea leida
+			String peticion = buffReader.readLine();
+			// Yoda reinterpreta el mensaje:
+			String respuesta=yodaDo(peticion);
+			
+			// Enviamos la traducción de Yoda:
+            printWriter.println(respuesta);		
+			
+		} catch (IOException e) {
+			System.err.println("Error al obtener los flujso de entrada/salida.");
+		}
+
+	}
+
+	// Yoda interpreta una frase y la devuelve en su "dialecto":
+	private String yodaDo(String peticion) {
+		// Desordenamos las palabras:
+		String[] s = peticion.split(" ");
+		String resultado="";
+		
+		for(int i=0;i<s.length;i++){
+			int j=random.nextInt(s.length);
+			int k=random.nextInt(s.length);
+			String tmp=s[j];
+			
+			s[j]=s[k];
+			s[k]=tmp;
+		}
+		
+		resultado=s[0];
+		for(int i=1;i<s.length;i++){
+		  resultado+=" "+s[i];
+		}
+		
+		return resultado;
+	}
+}
---
+++ Practica 2/Ejercicio4/YodafyServidorIterativo.java
@@ -0,0 +1,44 @@
+package Yodafy;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+//
+// YodafyServidorIterativo
+// (CC) jjramos, 2012
+//
+public class YodafyServidorIterativo {
+
+	public static void main(String[] args) {
+	
+		// Puerto de escucha
+		int port=8989;
+		
+		try {
+			// Abrimos el socket en modo pasivo, escuchando el en puerto indicado por "port"
+			ServerSocket serverSocket = new ServerSocket(port);
+			
+			// Mientras ... siempre!
+			do {
+				
+				// Aceptamos una nueva conexión con accept()
+				Socket socket = serverSocket.accept();
+				
+				// Creamos un objeto de la clase ProcesadorYodafy, pasándole como 
+				// argumento el nuevo socket, para que realice el procesamiento
+				// Este esquema permite que se puedan usar hebras más fácilmente.
+				ProcesadorYodafy procesador=new ProcesadorYodafy(socket);
+				procesador.start();
+				
+			} while (true);
+			
+		} catch (IOException e) {
+			System.err.println("Error al escuchar en el puerto "+port);
+		}
+
+	}
+
+}
